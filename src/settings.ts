59e354aeed6bdf4c34efc02cfba6708cb37489a3
cefali <lucas.cefali@gmail.com>
2023-09-29 08:18:12 -0300

    fix(linux): network mode

--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 export const NODE_ENV = process.env.NODE_ENV ?? 'production';
 export const DATABASE_CLIENT = process.env.DATABASE_CLIENT ?? 'pg';
 export const DATABASE_URL = process.env.DATABASE_URL ?? 'postgresql://postgres:mysecretpassword@localhost:5432/postgres';
-export const PORT = Number(process.env.PORT) ?? 3000;
+export const PORT = Number(process.env.PORT ?? 3000) ;
 export const ADDRESS = process.env.ADDRESS ?? undefined;
 export const API_INSTANCE = process.env.API_INSTANCE ?? 'api1';
 export const CLUSTER_WORKERS = Number(process.env.CLUSTER_WORKERS ?? 5);
